7d0b6c1d61f32fc586d85c49ebd932cca1bb3960
Brian Kleiboeker <bkleiboeker@wustl.edu>
2023-04-29 09:32:18 -0500

    fix error maybe

--- R/assign_structures.R
+++ R/assign_structures.R
@@ -79,14 +79,13 @@ assign_structures <- function(comp,
 
   c_tot <- (extract_num_elements_internal("C",comp) + extract_num_elements_internal(carbon_isotope_symbol,comp))
   h_tot <- (extract_num_elements_internal("H",comp) + extract_num_elements_internal(hydrogen_isotope_symbol,comp))
+  o <- extract_num_elements_internal("O",comp)
+  n <- extract_num_elements_internal("N",comp)
+  p <- extract_num_elements_internal("P",comp)
+  na <- extract_num_elements_internal("Na",comp)
+  cl <- extract_num_elements_internal("Cl",comp)
   result <- mapply(assign_species,
-         c_tot,
-         h_tot,
-         extract_num_elements_internal("O",comp),
-         extract_num_elements_internal("N",comp),
-         extract_num_elements_internal("P",comp),
-         extract_num_elements_internal("Na",comp),
-         extract_num_elements_internal("Cl",comp),
+         c_tot,h_tot,o,n,p,na,cl,
          ion.mode,
          rep(list(adducts),length(c)),
          ( c - (h/2) + ((n+p)/2) +1 ),
